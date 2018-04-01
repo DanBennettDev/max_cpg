958e11c85bcce926e5a98872a3ad69d4a8f1f17b
danBennettDev <skjolbrot@gmail.com>
2018-04-01 09:35:15 +0100

    code tidy, fixed extra inlets

--- source/projects/cpg.network/cpg.network.cpp
+++ source/projects/cpg.network/cpg.network.cpp
@@ -9,8 +9,10 @@
 
 	TODO:
 
-		- write new operator
-		- add controls for 
+		- add signal inputs for freq
+
+
+		- add param controls for 
 			- delay out change
 
 		Does weight scaling work correctly?
@@ -48,25 +50,25 @@ using namespace c74::min;
 class cpg_net : public object<cpg_net>, public vector_operator<>
 {
 private:
-	std::shared_ptr<MatsuokaEngine> engine_ptr;
-	MatsuNode dummyNode;
-	double freqComp{ DEFAULTFREQCOMPENSAITON };
-	int local_srate;
+	std::shared_ptr<MatsuokaEngine> _engine_ptr;
+	MatsuNode _dummyNode;
+	double _freqComp{ DEFAULTFREQCOMPENSAITON };
+	int _local_srate;
 
 	// holds raw output values for interpolation. Barebones ringbuffer approach.
-	sample outRingBuff[MAX_NODES][INTERP_SAMPLES]{ 0 };
-	int ringIndex{ 0 };
-	number freqs[MAX_NODES]{ 0 };
-	number phase{ 0 };
-	number phaseStep;
+	sample _outRingBuff[MAX_NODES][INTERP_SAMPLES]{ 0 };
+	int _ringIndex{ 0 };
+	number _freqs[MAX_NODES]{ 0 };
+	number _phase{ 0 };
+	number _phaseStep;
 
-	lib::interpolator::hermite<sample> interp_herm;
-	lib::interpolator::linear<sample> interp_lin;
-	bool m_initialized{ false };
-	int nodeCount{ 1 };
+	lib::interpolator::hermite<sample> _interp_herm;
+	lib::interpolator::linear<sample> _interp_lin;
+	bool _initialized{ false };
+	int _nodeCount{ 1 };
 
-	vector< unique_ptr<inlet<>> >			inlets;				///< this object's inlets
-	vector< unique_ptr<outlet<>> >			outlets;				///< this object's outlets
+	vector< unique_ptr<inlet<>> >			ins;				///< this object's ins
+	vector< unique_ptr<outlet<>> >			outs;				///< this object's outs
 
 public:
 
@@ -83,20 +85,20 @@ public:
 	argument<int> sample_rate_arg{ this, "sample_rate", "The sample rate of network calculations." };
 	argument<int> tRatio_arg{ this, "tRatio_param", "The ratio of equation params t2:t1." };
 	argument<int> c_arg{ this, "c_param", "The equation parameter c." };
-	argument<int> b_arg{ this, "c_param", "The equation parameter b." };
-	argument<int> g_arg{ this, "c_param", "The equation parameter g." };
+	argument<int> b_arg{ this, "b_param", "The equation parameter b." };
+	argument<int> g_arg{ this, "g_param", "The equation parameter g." };
 
 
 
 	cpg_net(const atoms& args = {})
 	{
-		m_initialized = false;
+		_initialized = false;
 
 		cout << "started" << endl;
 
 		// gather info for basic object init
 		if (args.size() > 0) {
-			nodeCount = args[0];
+			_nodeCount = args[0];
 		}
 
 		if (args.size() > 1) {
@@ -106,70 +108,65 @@ public:
 				cout << "sample rate must be system sample rate, or else >0 and < system sample-rate / 2: " << srate_in << endl;
 				cout << "setting to system sample rate" << endl;
 
-				local_srate = (int)samplerate();
+				_local_srate = (int)samplerate();
 			}
 			else {
-				local_srate = srate_in;
+				_local_srate = srate_in;
 			}
 		}
 		else {
-			local_srate = (int)samplerate();
+			_local_srate = (int)samplerate();
 		}
 
-		engine_ptr = std::shared_ptr<MatsuokaEngine>(
-			new  MatsuokaEngine(local_srate, true, false, true));
-		dummyNode = MatsuNode();
+		_engine_ptr = std::shared_ptr<MatsuokaEngine>(
+			new  MatsuokaEngine(_local_srate, true, false, true));
+		_dummyNode = MatsuNode();
 
 		// will be adding more (replacing this) later
-		inlets.push_back(std::make_unique<inlet<>>(this, "(messages) control input"));
+		ins.push_back(std::make_unique<inlet<>>(this, "(messages) control input"));
 
-		// set up nodes and inlets/outlets for them
-		for (int nodeID = 0; nodeID < nodeCount; ++nodeID) {
-			//inlets.push_back(std::make_unique<inlet<>>(this, "(signal) audio input"));
-			outlets.push_back(std::make_unique<outlet<>>(this, "(signal) audio output " + nodeID, "signal"));
+		// set up nodes and ins/outs for them
+		for (int nodeID = 0; nodeID < _nodeCount; ++nodeID) {
+			//ins.push_back(std::make_unique<inlet<>>(this, "(signal) audio input"));
+			outs.push_back(std::make_unique<outlet<>>(this, "(signal) audio output " + nodeID, "signal"));
 			if (nodeID != 0) {
-				engine_ptr->addChild(0, nodeID);
+				_engine_ptr->addChild(0, nodeID);
 			}
 		}
 		// connect all nodes to all others, but with 0 signal weight
-		for (int nodeID = 0; nodeID < nodeCount; ++nodeID) {
-			for (int connectToID = 0; connectToID < nodeCount; ++connectToID) {
+		for (int nodeID = 0; nodeID < _nodeCount; ++nodeID) {
+			for (int connectToID = 0; connectToID < _nodeCount; ++connectToID) {
 				if (nodeID != connectToID) {
-					engine_ptr->setConnection(nodeID, connectToID, 0.0);
+					_engine_ptr->setConnection(nodeID, connectToID, 0.0);
 				}
 			}
 		}
 
-		engine_ptr->setUnityConnectionWeight(UNITY_CONN_WEIGHT);
-		engine_ptr->setConnectionWeightScaling(true);
-		engine_ptr->setParam_t2Overt1(T2_INIT / T1_INIT);
-		engine_ptr->setParam_c(C_INIT);
-		engine_ptr->setParam_b(B_INIT);
-		engine_ptr->setParam_g(G_INIT);
-		engine_ptr->setFreqCompensation(DEFAULTFREQCOMPENSAITON);
-		engine_ptr->setNodeFrequency(0u, 1.0, true);
+		_engine_ptr->setUnityConnectionWeight(UNITY_CONN_WEIGHT);
+		_engine_ptr->setConnectionWeightScaling(true);
+		_engine_ptr->setParam_t2Overt1(T2_INIT / T1_INIT);
+		_engine_ptr->setParam_c(C_INIT);
+		_engine_ptr->setParam_b(B_INIT);
+		_engine_ptr->setParam_g(G_INIT);
+		_engine_ptr->setFreqCompensation(DEFAULTFREQCOMPENSAITON);
+		_engine_ptr->setNodeFrequency(0u, 1.0, true);
 
-		dummyNode.set_t1(T1_INIT);
-		dummyNode.set_t2(T2_INIT);
-		dummyNode.set_c(C_INIT);
-		dummyNode.set_b(B_INIT);
-		dummyNode.set_g(G_INIT);
+		_dummyNode.set_t1(T1_INIT);
+		_dummyNode.set_t2(T2_INIT);
+		_dummyNode.set_c(C_INIT);
+		_dummyNode.set_b(B_INIT);
+		_dummyNode.set_g(G_INIT);
 
 		setParams(args, true);
 
-		engine_ptr->calibrate();
+		_engine_ptr->calibrate();
 		calibrate.set();
 
 		cout << "initialised network" << endl;
 
-		m_initialized = true;
+		_initialized = true;
 	}
 
-
-	inlet<>			in{ this, "(signal) input" };
-	inlet<>			freq{ this, "(signal) node frequency" };
-
-	outlet<>		out{ this, "(signal) ramp wave", "signal" };
 
 
 	//argument<number> a_t1 { this, "t1", "equation parameter t1",
@@ -181,8 +178,8 @@ public:
 
 	message<> bang{ this, "bang", "reset the node",
 		MIN_FUNCTION{
-		if (m_initialized) {
-			engine_ptr->reset();
+		if (_initialized) {
+			_engine_ptr->reset();
 		}
 	return {};
 	}
@@ -200,7 +197,7 @@ public:
 	message<> weight{ this, "weight",
 		MIN_FUNCTION{
 		if (args.size() >= 3) {
-			engine_ptr->setConnection((int)args[0],(int)args[1], (double)args[2]);
+			_engine_ptr->setConnection((int)args[0],(int)args[1], (double)args[2]);
 			}
 	return {};
 	}
@@ -209,7 +206,7 @@ public:
 	message<> offset{ this, "offset",
 		MIN_FUNCTION{
 		if (args.size() >= 3) {
-			engine_ptr->setConnectionPhaseOffset((int)args[0],(int)args[1], (double)args[2]);
+			_engine_ptr->setConnectionPhaseOffset((int)args[0],(int)args[1], (double)args[2]);
 		}
 	return {};
 	}
@@ -218,8 +215,8 @@ public:
 	message<> node_freq{ this, "node_freq",
 		MIN_FUNCTION{
 		if (args.size() >= 2){
-			freqs[(int)args[0]] = args[1];
-			engine_ptr->setNodeFrequency((int)args[0],(int)args[1], false);
+			_freqs[(int)args[0]] = args[1];
+			_engine_ptr->setNodeFrequency((int)args[0],(int)args[1], false);
 		}
 	return {};
 	}
@@ -228,11 +225,11 @@ public:
 	message<> net_freq{ this, "net_freq",
 		MIN_FUNCTION{
 		if (!args.empty()) {
-			number freqMult = (float)args[0] / freqs[0];
-			freqs[0] = args[0];
-			engine_ptr->setNodeFrequency(0,(int)args[0], true);
-			for (int i = 1; i < nodeCount; i++) {
-				freqs[i] *= freqMult;
+			number freqMult = (float)args[0] / _freqs[0];
+			_freqs[0] = args[0];
+			_engine_ptr->setNodeFrequency(0,(int)args[0], true);
+			for (int i = 1; i < _nodeCount; i++) {
+				_freqs[i] *= freqMult;
 			}
 		}
 	return {};
@@ -250,7 +247,7 @@ public:
 
 	//attribute<number> frequency { this, "frequency" , 1.0, description{"Frequency in Hz"},
 	//	setter { MIN_FUNCTION {
-	//		if (m_initialized) {
+	//		if (_initialized) {
 	//			m_oscillator.frequency(args[0], samplerate());
 	//		}
 	//		return args;
@@ -265,11 +262,11 @@ public:
 		// For each frame in the vector calc each channel
 
 		for (auto frame = 0; frame<input.frame_count(); ++frame) {
-			engine_ptr->doQueuedActions();
-			engine_ptr->step();
+			_engine_ptr->doQueuedActions();
+			_engine_ptr->step();
 			// send to max output
-			for (int channel = 0; channel < nodeCount; ++channel) {
-				output.samples(channel)[frame] = outRingBuff[channel][ringIndex];
+			for (int channel = 0; channel < _nodeCount; ++channel) {
+				output.samples(channel)[frame] = _outRingBuff[channel][_ringIndex];
 			}
 		}
 	}
@@ -282,38 +279,38 @@ public:
 
 		for (auto frame = 0; frame<input.frame_count(); ++frame) {
 
-			// if phase wraps, calc sample from network
-			phase += phaseStep;
-			if (phase > 1.0) {
+			// if _phase wraps, calc sample from network
+			_phase += _phaseStep;
+			if (_phase > 1.0) {
 
-				ringIndex++;
-				if (ringIndex >= INTERP_SAMPLES) { ringIndex = 0; }
-				engine_ptr->doQueuedActions();
-				engine_ptr->step();
+				_ringIndex++;
+				if (_ringIndex >= INTERP_SAMPLES) { _ringIndex = 0; }
+				_engine_ptr->doQueuedActions();
+				_engine_ptr->step();
 			}
-			else if (phase < 0.0) { // sholdn't happen
-				phase += 1.0;
+			else if (_phase < 0.0) { // sholdn't happen
+				_phase += 1.0;
 			}
 
-			// regardless of phase wrap, calculate our interpolated sample for outputs
-			for (int channel = 0; channel < nodeCount; ++channel) {
-				outRingBuff[channel][ringIndex] = engine_ptr->getNodeOutput(channel);
+			// regardless of _phase wrap, calculate our interpolated sample for outputs
+			for (int channel = 0; channel < _nodeCount; ++channel) {
+				_outRingBuff[channel][_ringIndex] = _engine_ptr->getNodeOutput(channel);
 
-				if (local_srate > 11024) {
+				if (_local_srate > 11024) {
 					output.samples(channel)[frame] =
-							interp_herm(interpRingLookup(outRingBuff[channel], ringIndex,-3),
-										interpRingLookup(outRingBuff[channel], ringIndex, -2),
-										interpRingLookup(outRingBuff[channel], ringIndex, -1),
-										outRingBuff[channel][ringIndex],
-										phase);
+							_interp_herm(interpRingLookup(_outRingBuff[channel], _ringIndex,-3),
+										interpRingLookup(_outRingBuff[channel], _ringIndex, -2),
+										interpRingLookup(_outRingBuff[channel], _ringIndex, -1),
+										_outRingBuff[channel][_ringIndex],
+										_phase);
 				}
 				else {
 					output.samples(channel)[frame] =
-						interp_lin(interpRingLookup(outRingBuff[channel], ringIndex, -3),
-							interpRingLookup(outRingBuff[channel], ringIndex, -2),
-							interpRingLookup(outRingBuff[channel], ringIndex, -1),
-							outRingBuff[channel][ringIndex],
-							phase);
+						_interp_lin(interpRingLookup(_outRingBuff[channel], _ringIndex, -3),
+							interpRingLookup(_outRingBuff[channel], _ringIndex, -2),
+							interpRingLookup(_outRingBuff[channel], _ringIndex, -1),
+							_outRingBuff[channel][_ringIndex],
+							_phase);
 				}
 				
 			}
@@ -326,10 +323,10 @@ public:
 	/// Max takes care of squashing denormal for us by setting the FTZ bit on the CPU.
 	void operator()(audio_bundle input, audio_bundle output) 
 	{
-		if (m_initialized) {
-			phaseStep = local_srate / samplerate();
+		if (_initialized) {
+			_phaseStep = _local_srate / samplerate();
 
-			if (local_srate == (int)samplerate()) {
+			if (_local_srate == (int)samplerate()) {
 				calcVector_nonInterp(input, output);
 			}
 			else {
@@ -345,16 +342,16 @@ public:
 	//void calibrate()
 	//{
 	//	// calibrate nodes 
-	//	int settleTime = local_srate * 2;
-	//	dummyNode.setFrequency(CALIBRATION_CYCLES, local_srate);
+	//	int settleTime = _local_srate * 2;
+	//	_dummyNode.setFrequency(CALIBRATION_CYCLES, _local_srate);
 
 	//	while (settleTime-- > 0) {
-	//		dummyNode.doCalcStep(true, true);
+	//		_dummyNode.doCalcStep(true, true);
 	//		node.doCalcStep(true, true);
 	//	}
-	//	freqComp = dummyNode.calcFreqCompensation(CALIBRATION_CYCLES, local_srate);
-	//	node.setFreqCompensation(freqComp);
-	//	node.setFrequency(_freq, local_srate);
+	//	_freqComp = _dummyNode.calcFreqCompensation(CALIBRATION_CYCLES, _local_srate);
+	//	node.setFreqCompensation(_freqComp);
+	//	node.setFrequency(_freq, _local_srate);
 	//}
 
 
@@ -362,24 +359,24 @@ public:
 
 	void doCalibration()
 	{
-		m_initialized = false;
+		_initialized = false;
 		// calibrate nodes 
-		int settleTime = local_srate * 2;
-		dummyNode.setFrequency(CALIBRATION_CYCLES, local_srate);
-		dummyNode.reset();
+		int settleTime = _local_srate * 2;
+		_dummyNode.setFrequency(CALIBRATION_CYCLES, _local_srate);
+		_dummyNode.reset();
 		while (settleTime-- > 0) {
-			dummyNode.doCalcStep(true, true);
+			_dummyNode.doCalcStep(true, true);
 		}
-		freqComp = dummyNode.calcFreqCompensation(CALIBRATION_CYCLES, local_srate);
+		_freqComp = _dummyNode.calcFreqCompensation(CALIBRATION_CYCLES, _local_srate);
 
-		engine_ptr->setFreqCompensation(freqComp);
+		_engine_ptr->setFreqCompensation(_freqComp);
 
-		for (int i = 0; i < nodeCount; i++) {
-			engine_ptr->setNodeFrequency(i, freqs[i], false);
+		for (int i = 0; i < _nodeCount; i++) {
+			_engine_ptr->setNodeFrequency(i, _freqs[i], false);
 		}
-		engine_ptr->reset();
+		_engine_ptr->reset();
 	
-		m_initialized = true;
+		_initialized = true;
 	}
 
 
@@ -389,22 +386,22 @@ public:
 		int firstParam = startup ? 2 : 0;
 
 		if (args.size() > firstParam) {
-			engine_ptr->setParam_t2Overt1(args[firstParam]);
-			dummyNode.set_t2_over_t1(args[firstParam]);
+			_engine_ptr->setParam_t2Overt1(args[firstParam]);
+			_dummyNode.set_t2_over_t1(args[firstParam]);
 		}
 		if (args.size() > firstParam + 1) {
-			engine_ptr->setParam_c(args[firstParam + 1]);
-			dummyNode.set_c(args[firstParam + 1]);
+			_engine_ptr->setParam_c(args[firstParam + 1]);
+			_dummyNode.set_c(args[firstParam + 1]);
 		}
 
 		if (args.size() > firstParam + 2) {
-			engine_ptr->setParam_b(args[firstParam + 2]);
-			dummyNode.set_b(args[firstParam + 2]);
+			_engine_ptr->setParam_b(args[firstParam + 2]);
+			_dummyNode.set_b(args[firstParam + 2]);
 		}
 
 		if (args.size() > firstParam + 3) {
-			engine_ptr->setParam_g(args[firstParam + 3]);
-			dummyNode.set_g(args[firstParam + 3]);
+			_engine_ptr->setParam_g(args[firstParam + 3]);
+			_dummyNode.set_g(args[firstParam + 3]);
 		}
 	}
 
